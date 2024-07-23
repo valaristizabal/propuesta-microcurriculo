86078346f9881153ee4dcd9c9506c5369b7412e7
Valeria <119232403+valaristizabal@users.noreply.github.com>
2024-07-23 18:09:38 -0500

    creacion proyecto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!-- resources/views/people/index.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Personas</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+<body>
+    <div>
+        
+    </div>
+</body>
+</html>
